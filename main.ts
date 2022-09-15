fcc274ea24662714326f24bc43f767cb586cfe4e
Danie-u123 <113040612+Danie-u123@users.noreply.github.com>
2022-09-15 10:41:21 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    num_ += 9
+    basic.showNumber(num_)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    pic.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    num_ += 1
+    basic.showNumber(num_)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    pic.change(LedSpriteProperty.X, 1)
+})
+let pic: game.LedSprite = null
+let num_ = 0
 basic.showIcon(IconNames.Ghost)
-let pic = game.createSprite(0, 0)
-let num_ = 7
+num_ = 9
 basic.showNumber(num_)
+pic = game.createSprite(2, 2)
 basic.forever(function () {
 	
 })
